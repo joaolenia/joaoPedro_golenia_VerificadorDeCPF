1665506d7bfb4ff6de87ef02eea5268ed99879b4
Joao Pedro <joaopedrogolenia@gmail.com>
2024-04-09 15:53:57 -0300

    feat: Adicionando a estrutura das clases CPF e CPFValidador

---
+++ src/main/java/funcionalidades/CPF.java
@@ -0,0 +1,17 @@
+package funcionalidades;
+
+import java.util.List;
+
+public class CPF {
+    private List<String> cpfs;
+
+    public CPF(List<String> cpfs) {
+        this.cpfs = cpfs;
+    }
+
+    public List<String> getCpfs() {
+        return cpfs;
+    }
+}
+
+
---
+++ src/main/java/funcionalidades/CPFValidador.java
@@ -0,0 +1,21 @@
+package funcionalidades;
+
+import java.util.List;
+
+public class CPFValidador {
+
+    private List<String> cpfsvalidos;
+    private List<String> cpfsinvalidos;
+
+    public CPFValidador(){}
+
+    public void TamanhoCPF(CPF cpfs){
+
+    }
+
+    public void Imprimir(){
+
+
+    }
+
+}
