2b1fff6b131882ac7fff5c5030b02f1cb97a7ddc
Joao Pedro <joaopedrogolenia@gmail.com>
2024-04-09 16:02:43 -0300

    feat: Adicionando o metodo 'ler' na main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,29 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
 
+    }
+    public static void ler(String caminho, List<String> cpfs){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(caminho));
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                linha = linha.trim();
+                if (!linha.isEmpty()) {
+                    cpfs.add(linha);
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
     }
 }
 
